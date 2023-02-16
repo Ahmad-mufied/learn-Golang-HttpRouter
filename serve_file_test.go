32d4bc1669a2bb841c51c440d63d300d616467fb
Ahmad-mufied <teranggelap01@gmail.com>
2023-02-16 17:34:15 +0700

    trying to implement serve_file for serving some static files

---
+++ serve_file_test.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"embed"
+	"io"
+	"io/fs"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/stretchr/testify/assert"
+)
+
+/*
+
+== Serve File
+
+-	Pada materi Go-Lang Web, kita sudah pernah membahas tentang Serve File
+-	Pada Router pun, mendukung serve static file menggunakan function ServeFiles(Path, FileSystem)
+-	Dimana pada Path, kita harus menggunakan Catch All Parameter
+-	Sedangkan pada FileSystem kita bisa melakukan manual load dari folder atau menggunakan golang embed, seperti yang pernah kita bahas di materi Go-Lang Web
+
+*/
+
+//go:embed resources
+var resources embed.FS
+
+func TestServeFile(t *testing.T) {
+	router := httprouter.New()
+	directory, _ := fs.Sub(resources, "resources")
+	router.ServeFiles("/files/*filepath", http.FS(directory))
+
+	request := httptest.NewRequest("GET", "http://localhost:3000/files/hello.txt", nil)
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	assert.Equal(t, "Hello HttpRouter", string(body))
+}
+
+func TestServeFileGoodBye(t *testing.T) {
+	router := httprouter.New()
+	directory, _ := fs.Sub(resources, "resources")
+	router.ServeFiles("/files/*filepath", http.FS(directory))
+
+	request := httptest.NewRequest("GET", "http://localhost:3000/files/goodbye.txt", nil)
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	assert.Equal(t, "Good Bye HttpRouter", string(body))
+}
